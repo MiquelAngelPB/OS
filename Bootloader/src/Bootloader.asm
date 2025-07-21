7a1581ea34781c5ddf207b1c2069d09ae8813741
MiquelAngelPB <miquelapb29@gmail.com>
2025-07-21 23:17:28 +0200

    The kernel can now be loaded

--- Bootloader/src/Bootloader.asm
+++ Bootloader/src/Bootloader.asm
@@ -26,79 +26,26 @@ loadkernel:
     mov dh, 0       ;head
     mov dl, [bootdrive] ;disk, will use the one that the BIOS gives you
     mov bx, 0x7e00  ;destination bx:es
-    mov cx, 0x0000  ;temporal value for es
-    mov es, cx      ;destination bx:es
-    int 0x13
+    push ax
+    mov ax, 0x0000  ;temporal value for es
+    mov es, ax      ;destination bx:es
+    pop ax
+    int 0x13        ;Check: https://www.ascii-codes.com/ and https://es.wikipedia.org/wiki/Int_13h
 
     jc diskerror         ;jump if carry flag is set (if there is a disk error)
 
-    ; ================================================
-    ; Debugging
-    ; ================================================
-
-    ;jmp 0x0000:0x1000   ;jump to kernel
-
-    mov ah, 0x0e         
-    mov bx, 0x7dfe
-
-    ;ignore this one
-    ;Write the int 13h error code. Check: https://www.ascii-codes.com/ and https://es.wikipedia.org/wiki/Int_13h
-    ;mov al, bh
-    ;int 0x10
-
-    ;Display 0x55
-    mov al, [es:bx]
-    int 0x10
-
-    ;Display 0xAA
-    inc bx
-    mov al, [es:bx]
-    int 0x10
-
-    ;Display a few characters from the 2nd sector (SHould be CCCCCCC...)
-    inc bx
-    mov al, [es:bx]
-    int 0x10
-
-    inc bx
-    mov al, [es:bx]
-    int 0x10
-
-    inc bx
-    mov al, [es:bx]
-    int 0x10
-
-    inc bx
-    mov al, [es:bx]
-    int 0x10
-
-    inc bx
-    mov al, [es:bx]
-    int 0x10
-
-    inc bx
-    mov al, [es:bx]
-    int 0x10
-
-    inc bx
-    mov al, [es:bx]
-    int 0x10
-
-    inc bx
-    mov al, [es:bx]
-    int 0x10
-
+    push bx
     mov bx, kernelmsg    ;display that the kernel has been loaded into memory
     call printstr
+    pop bx
+
+    jmp 0x0000:0x7e00   ;jump to kernel
 
     jmp halt            ;do nothing for eternity and beyond
 
 diskerror:              ;display disk error message
     mov bx, errmsg
     call printstr
-
-    mov al, [0x7e00]
-    int 0x10
 
     jmp halt
 
